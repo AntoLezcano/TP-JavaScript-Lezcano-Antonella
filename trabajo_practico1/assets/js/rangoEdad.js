be9f4be8b502bf65fb1827cce56075f491fc8f3d
Antonella <antonella.beti@gmail.com>
2024-05-10 10:01:34 -0300

    se crearon dos carpetas, un index y dos archivos js

--- trabajo_practico1/assets/js/rangoEdad.js
+++ trabajo_practico1/assets/js/rangoEdad.js
@@ -2,18 +2,15 @@ let edad =  prompt("ingrese su edad");
 /* estructura de control para mostrar el rango de edad */
 if (edad >= 0 && edad <=12 ){
 console.log("su rango de edad ´niño´")
-} 
+} else
 if (edad  >= 13 && edad <= 19 ){
     console.log("su rango de edad es ´adolescente´")
-}
+} else
 if (edad >= 20 && edad <=59 ){
     console.log("su rango de edad ´adulto´")
-    } 
+    } else
     if (edad  >= 60 && 120<= edad ){
         console.log("su rango de edad es ´adulto mayor´")
-    } else {
-        console.log("probablemente esta muerto")
-    }
-    
+    } 
 
 
