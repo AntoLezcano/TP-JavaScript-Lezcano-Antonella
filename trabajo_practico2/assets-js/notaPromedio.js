6802bb83cba49d1173ad5ccc693fa37d91e94398
Antonella <antonella.beti@gmail.com>
2024-05-10 12:52:56 -0300

    se agrego una estructura de control en notaPromedio

---
+++ trabajo_practico2/assets-js/notaPromedio.js
@@ -0,0 +1,17 @@
+let cuat1 = parseFloat(prompt ("Ingrese la nota del primer cuatrimestre"));
+let cuat2 = parseFloat(prompt ("Ingrese la nota del segundo cuatrimestre"));
+let cuat3 = parseFloat(prompt ("Ingrese la nota del tercer cuatrimestre"));
+let promedio
+promedio = (cuat1+cuat2+cuat3)/3
+
+if (promedio>=0 && promedio<=3){
+    console.log("Nota Insuficiente")
+}else if (promedio>=4 && promedio<=5){
+    console.log("Nota Regular")
+}else if (promedio>=6 && promedio<=7) {
+    console.log("Nota Buena")
+} else if (promedio>=8 && promedio<=9){
+    console.log("Nota muy Buena")
+} else {
+    console.log("Nota Sobresaliente " )
+}
