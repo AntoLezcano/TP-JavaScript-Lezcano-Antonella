4cfb74eb12e768af668eb6f885f23675c8a5e988
Antonella <antonella.beti@gmail.com>
2024-05-24 09:54:49 -0300

    se agrego una estructura de control y se editaron algunas funciones

--- trabajo_practico3/assets/js/apps.js
+++ trabajo_practico3/assets/js/apps.js
@@ -12,20 +12,26 @@ function agregarTarea(){
 }
 
 function editarTarea() {
-    if (tareas.length) {
-        let opcion = parseInt(prompt("Ingrese el número de la tarea que desea editar (1 al " + tareas.length + "): ")) - 1;
-        if (opcion >= 0 && opcion < tareas.length) {
-            tareas[opcion] = prompt("Ingrese la nueva descripción de la tarea:");
-            alert("El listado de tareas quedo: \n" + tareas.join("\n"));
-        } else if (opcion > tareas.length ) {
-            alert("Índice no válido");
+    let nombreEditar = prompt("Ingrese el nombre de la materia que busca editar: ");
+        
+        let index = tareas.indexOf(nombreEditar);
+        
+        
+        if (index !== -1) {
+            
+            let tareaNueva = prompt("Ingrese la tarea editada")
+            tareas[index] = tareaNueva;
+            console.log("Lista de tareas editada : "+ tareas);
         } else {
-        alert("No hay tareas para editar");
-    }
+            console.log("El elemento especificado no se encuentra en la lista de tareas");
+        }
+    
 }
-}
-function editarTarea() {
-    let modificar = tareas.in
+
+function listaTarea() {
+    for (let i = 0; i < tareas.length; i++)
+        {
+        console.log(i+ " " +tareas[i]);}
 
 }
 
@@ -44,4 +50,28 @@ alert("La tarea fue eliminada \n "+ "la lista de tareas quedo de la siguiente ma
     alert("No hay tareas para eliminar");
 }
 }
+let cerrar = true
+while (cerrar) {
+    let opciones = parseInt(prompt("Ingrese una de las opciones \n 1 Agregar Tarea \n 2 Listar Tareas \n 3 Editar Tarea \n 4 Eliminar Tarea \n 5 Salir"));
 
+    switch(opciones) {
+        case 1:
+            agregarTarea();
+            break;
+        case 2:
+            listaTarea();
+            break;
+        case 3:
+            editarTarea();
+            break;
+        case 4:
+            eliminarTarea();
+            break;
+        case 5: 
+        cerrar = false
+        break;
+        default:
+            alert("Opción no válida");
+    }
+    
+}
